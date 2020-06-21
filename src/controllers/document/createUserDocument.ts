5861bcf2c55853123747670f5704445ed34916ee
Kelvin Modesto <km.modesto@gmail.com>
2020-06-21 02:53:03 -0300

    Finish list all and create requests

--- src/controllers/document/createUserDocument.ts
+++ src/controllers/document/createUserDocument.ts
@@ -1,3 +1,22 @@
-export default function createUserDocument() {
+import { ContextStrategy } from '@db/base';
+import crypto from 'crypto';
+import { RequestError } from '../../utils';
 
+export default function createUserDocument(router: any = {}, context: ContextStrategy) {
+  router.post('/', async (req: any, res: any, next: any) => {
+    try {
+      const { value } = req.body;
+      const userDocument = crypto.createHash('sha256').update(value.toString()).digest('base64');
+      const creditCardToken = crypto.createHash('sha256').update(value.toString()).digest('hex');
+
+      const { _id:id } = await context.create({ value, creditCardToken, userDocument });
+      if (res.status(200)) {
+        res.send({ value, creditCardToken, userDocument, id });
+      } else {
+        return new RequestError();
+      }
+    } catch(error) {
+      next(error);
+    }
+  });
 }
--- src/controllers/document/listUserDocuments.ts
+++ src/controllers/document/listUserDocuments.ts
@@ -1,3 +1,26 @@
-export default function listUserDocuments() {
+import * as _ from 'lodash';
+import { ContextStrategy } from '../../db';
+import { RequestError } from '../../utils';
 
+export default function listUserDocuments(router: any = {}, context: ContextStrategy) {
+  router.get('/', async (req: any, res: any, next: any) => {
+    try {
+      const { value } = req.query;
+      const list = await context.read(_.pick({ value }));
+      let data = list;
+      if (list.length > 0) {
+        data = list.map((it: any) => {
+          let { value, _id:id } = it;
+          return { value, id };
+        });
+      }
+      if (res.status(200)) {
+        res.send({ data });
+      }  else {
+        return new RequestError();
+      }
+    } catch(error) {
+      next(error)
+    }
+  });
 }
